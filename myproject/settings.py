8f060fd82400ed39a3ea122e3622bb312d5635da
GohilVidhi <gohilvidhi3@gmail.com>
2024-06-19 15:43:34 +0530

    set

--- myproject/settings.py
+++ myproject/settings.py
@@ -53,6 +53,11 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+]
 ROOT_URLCONF = 'myproject.urls'
 
 TEMPLATES = [
