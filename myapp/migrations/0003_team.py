770b7adf04cb86a179d3f1cafb5fe46324e2b945
GohilVidhi <153704388+GohilVidhi@users.noreply.github.com>
2024-06-20 16:18:42 +0530

    Update 0003_team.py
    h

--- myapp/migrations/0003_team.py
+++ myapp/migrations/0003_team.py
@@ -6,9 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('myapp', '0002_user'),
-    ]
+   
 
     operations = [
         migrations.CreateModel(
