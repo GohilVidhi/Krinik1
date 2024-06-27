deef34fced37bba8fcfde9c11d8d4d0c6d49fe18
GohilVidhi <153704388+GohilVidhi@users.noreply.github.com>
2024-06-27 11:26:22 +0530

    Update views.py
    fgh

--- myapp/views.py
+++ myapp/views.py
@@ -129,54 +129,54 @@ class Team_view(APIView):
 
 #---------------Player View----------------------
 
-# class Player_view(APIView):
-#     def get(self,request,id=None):  
-#         if id:
+class Player_view(APIView):
+    def get(self,request,id=None):  
+        if id:
         
-#             try:
-#                 uid=Player.objects.get(id=id)
-#                 serializer=Player_serializers(uid)
-#                 return Response({'status':'success','data':serializer.data})
-#             except:
-#                 return Response({'status':"Invalid"})
-#         else:
-#             uid=Player.objects.all().order_by("-id")
-#             serializer=Player_serializers(uid,many=True)
-#             return Response({'status':'success','data':serializer.data})
+            try:
+                uid=Player.objects.get(id=id)
+                serializer=Player_serializers(uid)
+                return Response({'status':'success','data':serializer.data})
+            except:
+                return Response({'status':"Invalid"})
+        else:
+            uid=Player.objects.all().order_by("-id")
+            serializer=Player_serializers(uid,many=True)
+            return Response({'status':'success','data':serializer.data})
       
-#     def post(self,request):
-#         serializer=Player_serializers(data=request.data)
-#         print(serializer)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response({'status':'success','data':serializer.data})
-#         else:
-#             return Response({'status':"invalid data"})
+    def post(self,request):
+        serializer=Player_serializers(data=request.data)
+        print(serializer)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status':'success','data':serializer.data})
+        else:
+            return Response({'status':"invalid data"})
         
      
-#     def patch(self,request,id=None):
-#         try:
-#             uid=Player.objects.get(id=id)
-#         except:
-#             return Response({'status':"invalid data"})
-#         serializer=Player_serializers(uid,data=request.data,partial=True)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response({'status':'success','data':serializer.data})
-#         else:
-#             return Response({'status':"invalid data"})
+    def patch(self,request,id=None):
+        try:
+            uid=Player.objects.get(id=id)
+        except:
+            return Response({'status':"invalid data"})
+        serializer=Player_serializers(uid,data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status':'success','data':serializer.data})
+        else:
+            return Response({'status':"invalid data"})
         
         
         
         
               
-#     def delete(self,request,id=None):
-#         if id:
-#             try:
-#                 uid=Player.objects.get(id=id)
-#                 uid.delete()
-#                 return Response({'status':'Deleted data'})
-#             except:
-#                 return Response({'status':"invalid id"})
-#         else:
-#             return Response({'status':"invalid data"})
+    def delete(self,request,id=None):
+        if id:
+            try:
+                uid=Player.objects.get(id=id)
+                uid.delete()
+                return Response({'status':'Deleted data'})
+            except:
+                return Response({'status':"invalid id"})
+        else:
+            return Response({'status':"invalid data"})
